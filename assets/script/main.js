5b86640bcdda0b2f95fcf36f391aacbb55d4d105
Angela <aes.caldas@gmail.com>
2022-02-05 19:58:36 -0300

    Feat: create module

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,11 +1,4 @@
-// example of how to get json info
+import chooseBrand from './chooseBrand.js'
 
-fetch('assets/database/seachem.json')
-  .then(response => response.json())
-  .then(data => {
-    for (let i = 0; i < data.length; i++) {
-      let productName = data[i].name
-      let productDosage = data[i].dosage.dose
-      console.log(productDosage)
-    }
-  })
+const brandSelect = document.querySelector('[data-products')
+brandSelect.addEventListener('change', chooseBrand)
